adec9f4a6564b23c65428938462d2558e30e052f
chenshuhang <437132124@qq.com>
2020-08-20 11:38:26 +0800

    feat: add mock

--- config/config.ts
+++ config/config.ts
@@ -1,11 +1,11 @@
 export default {
   appType: 'h5',
   mobileLayout: true,
-  proxy: {
-    '/api': {
-      target: 'https://pvp.qq.com/web201605/js/',
-      changeOrigin: true,
-      pathRewrite: { '^/api': '' },
-    },
-  },
+  // proxy: {
+  //   '/api': {
+  //     target: 'https://pvp.qq.com/web201605/js/',
+  //     changeOrigin: true,
+  //     pathRewrite: { '^/api': '' },
+  //   },
+  // },
 };
